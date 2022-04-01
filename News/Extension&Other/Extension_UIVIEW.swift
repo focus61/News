ca2188f59fcb1423b7536a3e04ae9e1b69680619
focus61 <focussashka161@gmail.com>
2022-04-01 23:48:31 +0300

    Update Extension_UIVIEW.swift

--- News/Extension&Other/Extension_UIVIEW.swift
+++ News/Extension&Other/Extension_UIVIEW.swift
@@ -7,37 +7,6 @@ extension UIView {
         }
     }
     
-    func addBottomBorder(color: UIColor = UIColor.red, margins: CGFloat = 0, borderLineSize: CGFloat = 1) {
-               let border = UIView()
-               border.backgroundColor = color
-               border.translatesAutoresizingMaskIntoConstraints = false
-               self.addSubview(border)
-               border.addConstraint(NSLayoutConstraint(item: border,
-                                                       attribute: .height,
-                                                       relatedBy: .equal,
-                                                       toItem: nil,
-                                                       attribute: .height,
-                                                       multiplier: 1, constant: borderLineSize))
-               self.addConstraint(NSLayoutConstraint(item: border,
-                                                     attribute: .bottom,
-                                                     relatedBy: .equal,
-                                                     toItem: self,
-                                                     attribute: .bottom,
-                                                     multiplier: 1, constant: 0))
-               self.addConstraint(NSLayoutConstraint(item: border,
-                                                     attribute: .leading,
-                                                     relatedBy: .equal,
-                                                     toItem: self,
-                                                     attribute: .leading,
-                                                     multiplier: 1, constant: margins))
-               self.addConstraint(NSLayoutConstraint(item: border,
-                                                     attribute: .trailing,
-                                                     relatedBy: .equal,
-                                                     toItem: self,
-                                                     attribute: .trailing,
-                                                     multiplier: 1, constant: margins))
-        }
-    
     func addRightBorder(with color: UIColor?, andWidth borderWidth: CGFloat) {
         let border = UIView()
         border.backgroundColor = color
